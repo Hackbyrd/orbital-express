d2afb6df8254477963482df4c86769359fb86a6b
Jonathan Chen <jonathan@Jonathans-Air.fios-router.home>
2023-07-20 21:04:59 -0400

    reformatted create-Admin-model

--- migrations/20230609205440-create-Admin-model.js
+++ migrations/20230609205440-create-Admin-model.js
@@ -1,220 +1,232 @@
 'use strict';
 
-// require helpers
+// built-in modules
+const bcrypt = require('bcrypt');
+
+// third-party
+const DataTypes = require('sequelize').DataTypes;
+
+// helpers
 const constants = require('../helpers/constants');
 const { randomString } = require('../helpers/logic');
 
+const attrs = {
+  id: {
+    type: DataTypes.BIGINT,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+
+  timezone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'UTC'
+  },
+
+  locale: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'en'
+  },
+
+  active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  },
+
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: constants.ADMIN_ROLE.ADMIN
+  },
+
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  // The unique email of the admin user
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+
+  // The unique phone of the admin user
+  phone: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+
+  // salt should be randomly generate
+  salt: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+    defaultValue: randomString({ len: 128, uppercase: true, numbers: true, special: true })
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  // A token to help facilitate changing passwords
+  passwordResetToken: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: true,
+    defaultValue: null
+  },
+
+  // When the new password will expire
+  passwordResetExpire: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    validate: {
+      isDate: true
+    }
+  },
+
+  // emails 
+  emailConfirmed: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+
+  emailConfirmationCode: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+
+  emailConfirmationCodeExpire: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    validate: {
+      isDate: true
+    }
+  },
+
+  emailConfirmationCodeAttempts: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+
+  resetEmail: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+
+  // phone
+  phoneConfirmed: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+
+  phoneConfirmationCode: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+
+  phoneConfirmationCodeExpire: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+    validate: {
+      isDate: true
+    }
+  },
+
+  phoneConfirmationCodeAttempts: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+
+  resetPhone: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue: null,
+  },
+
+  // Whether user has accepted terms or not
+  acceptedTerms: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  },
+
+  // The number of times the user has logged in
+  loginCount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+    validate: {
+      isInt: true
+    }
+  },
+
+  // The last time the user has logged in
+  lastLogin: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: null,
+    validate: {
+      isDate: true
+    }
+  },
+
+  deletedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: null,
+    validate: {
+      isDate: true,
+    },
+  },
+
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  },
+
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false,
+  }
+}
+    
 module.exports = {
-  up: (queryInterface, DataTypes) => {
-    return queryInterface.createTable('Admins', {
-      id: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-
-      timezone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'UTC'
-      },
-  
-      locale: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'en'
-      },
-  
-      active: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true
-      },
-
-      role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: constants.ADMIN_ROLE.ADMIN
-      },
-  
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-  
-      lastName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-  
-      // The unique email of the admin user
-      email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        validate: {
-          isEmail: true
-        }
-      },
-  
-      // The unique phone of the admin user
-      phone: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false
-      },
-  
-      // salt should be randomly generate
-      salt: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        defaultValue: randomString({ len: 128, uppercase: true, numbers: true, special: true })
-      },
-  
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-  
-      // A token to help facilitate changing passwords
-      passwordResetToken: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: true,
-        defaultValue: null
-      },
-  
-      // When the new password will expire
-      passwordResetExpire: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-        validate: {
-          isDate: true
-        }
-      },
-  
-      // emails 
-      emailConfirmed: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-      },
-  
-      emailConfirmationCode: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-  
-      emailConfirmationCodeExpire: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-        validate: {
-          isDate: true
-        }
-      },
-  
-      emailConfirmationCodeAttempts: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0
-      },
-  
-      resetEmail: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-  
-      // phone
-      phoneConfirmed: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
-      },
-  
-      phoneConfirmationCode: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-  
-      phoneConfirmationCodeExpire: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-        validate: {
-          isDate: true
-        }
-      },
-  
-      phoneConfirmationCodeAttempts: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0
-      },
-  
-      resetPhone: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        defaultValue: null,
-      },
-  
-      // Whether user has accepted terms or not
-      acceptedTerms: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: true
-      },
-  
-      // The number of times the user has logged in
-      loginCount: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 0,
-        validate: {
-          isInt: true
-        }
-      },
-  
-      // The last time the user has logged in
-      lastLogin: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        defaultValue: null,
-        validate: {
-          isDate: true
-        }
-      },
-
-      deletedAt: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        defaultValue: null,
-        validate: {
-          isDate: true,
-        },
-      },
-
-      createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-        allowNull: false,
-      },
-
-      updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-        allowNull: false,
-      },
+  async up(queryInterface) {
+    return await queryInterface.sequelize.transaction(async t => {
+      await queryInterface.createTable('Admins', attrs, { transaction: t });
     });
   },
 
-  down: queryInterface => {
-    return queryInterface.dropTable('Admins');
+  async down(queryInterface) {
+    return await queryInterface.sequelize.transaction(async t => {
+      return await queryInterface.dropTable('Admins', { transaction: t });
+    });
   },
 };
